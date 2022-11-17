22828c838dbbbdba45b532db2d2762a0f9179f9e
login <w0nderfly@live.com>
2022-11-17 10:02:41 +0500

    first commit

---
+++ src/main/java/com/example/hwspring3/HwSpring3Application.java
@@ -0,0 +1,13 @@
+package com.example.hwspring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpring3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSpring3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hwspring3/HwSpring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwspring3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpring3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
